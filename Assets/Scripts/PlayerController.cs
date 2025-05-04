51aa1590c71f7052ed58b52df7a83f5be6199854
anjwl <anjihoo@snu.ac.kr>
2025-05-04 20:22:46 +0900

    AD 연타 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public Vector3 center_of_mass;
     public float move_speed;
@@ -9,6 +11,10 @@ public class Player : MonoBehaviour
 
     private Vector3 player_move_dir;
     private Rigidbody my_rigidbody;
+    private KeyCode previous_input = KeyCode.None;
+    private float ad_speed = 0;
+
+    [SerializeField] private float drag, x_coeff, y_coeff, y_intercept;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +25,8 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        player_input();
+        player_move_input();
+        player_acceleration();
     }
 
     // Update is called once per frame
@@ -27,22 +34,37 @@ public class Player : MonoBehaviour
     {
         move();
     }
-    private void player_input() {
+
+    private void player_acceleration() {
+        if (Input.GetKeyDown(KeyCode.A) & previous_input != KeyCode.A) {
+            previous_input = KeyCode.A;
+            ad_speed += 1;
+        }
+        if (Input.GetKeyDown(KeyCode.D) & previous_input != KeyCode.D) {
+            previous_input = KeyCode.D;
+            ad_speed += 1;
+        }
+
+        ad_speed *= drag;
+        move_speed = y_intercept + Mathf.Sqrt(x_coeff * ad_speed) * y_coeff;
+    }
+
+    private void player_move_input() {
         Vector3 camera_forward = _camera.transform.forward;
         camera_forward -= new Vector3(0, camera_forward.y, 0);
         camera_forward = camera_forward.normalized;
 
         player_move_dir = Vector3.zero;
-        if (Input.GetKey(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.UpArrow)) {
             player_move_dir += camera_forward;
         }
-        if (Input.GetKey(KeyCode.S)) {
+        if (Input.GetKey(KeyCode.DownArrow)) {
             player_move_dir -= camera_forward;
         }
-        if (Input.GetKey(KeyCode.A)) {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
             player_move_dir -= Vector3.Cross(Vector3.up, camera_forward);
         }
-        if (Input.GetKey(KeyCode.D)) {
+        if (Input.GetKey(KeyCode.RightArrow)) {
             player_move_dir += Vector3.Cross(Vector3.up, camera_forward);
         }
     }
